babd927c2147e9c41a1765a5b28b092f3cba9b5d
Ayush kumar <ayushkayshab04@gmail.com>
2025-07-25 16:37:41 +0530

    final prod deployment

--- src/config/config.js
+++ src/config/config.js
@@ -1,2 +1,2 @@
-// export const baseUrl = 'https://pdfuploadbackend-q2gt.onrender.com'
-export const baseUrl = 'http://localhost:5000'
+export const baseUrl = 'https://pdfuploadbackend-q2gt.onrender.com'
+// export const baseUrl = 'http://localhost:5000'
